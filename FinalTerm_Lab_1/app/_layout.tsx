4e36135652d8be0e21a49e7536087010077403e5
Zihadul Islam <22-47087-1@student.aiub.edu>
2026-09-12 19:05:33 +0600

    Switch Week 6 snapshot to the required three-tab shell

--- FinalTerm_Lab_1/app/_layout.tsx
+++ FinalTerm_Lab_1/app/_layout.tsx
@@ -1,13 +1,3 @@
 import { Stack } from "expo-router";
 import { StudentsProvider } from "../context/students-context";
-
-export default function RootLayout() {
-  return (
-    <StudentsProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Student Directory" }} />
-        <Stack.Screen name="statistics" options={{ title: "Statistics" }} />
-      </Stack>
-    </StudentsProvider>
-  );
-}
+export default function RootLayout(){return <StudentsProvider><Stack><Stack.Screen name="(tabs)" options={{headerShown:false}}/></Stack></StudentsProvider>}
