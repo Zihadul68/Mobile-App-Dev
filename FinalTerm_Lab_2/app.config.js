39e93cc29a6d94fd3c4e7331753fcdca12b2a646
Zihadul Islam <22-47087-1@student.aiub.edu>
2026-09-12 19:04:11 +0600

    Complete Week 9 Expo config with custom assets and identifiers

--- FinalTerm_Lab_2/app.config.js
+++ FinalTerm_Lab_2/app.config.js
@@ -4,11 +4,31 @@ module.exports = ({ config }) => ({
   slug: "student-directory-final-lab-2",
   version: "1.0.0",
   orientation: "portrait",
+  icon: "./assets/icon.png",
   userInterfaceStyle: "light",
   scheme: "studentdirectoryfinal2",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#0D1F4E",
+  },
   experiments: {
     ...config.experiments,
     typedRoutes: true,
+  },
+  ios: {
+    ...config.ios,
+    supportsTablet: true,
+    bundleIdentifier: "com.zihadul.studentdirectory",
+  },
+  android: {
+    ...config.android,
+    package: "com.zihadul.studentdirectory",
+    adaptiveIcon: {
+      ...(config.android?.adaptiveIcon ?? {}),
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#0D1F4E",
+    },
   },
   extra: {
     ...config.extra,
